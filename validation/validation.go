64720151d99065f5bf834987dd8cadbe04a9d333
drScripts <nathanael.vd@gmail.com>
2023-02-06 11:56:42 +0700

    feat: add validation

---
+++ validation/validation.go
@@ -0,0 +1,28 @@
+package validation
+
+import "regexp"
+
+func AlphanumString(str string) bool {
+	regex, err := regexp.MatchString("^[A-Za-z0-9_-]*$", str)
+	if err != nil {
+		return false
+	}
+
+	return regex
+}
+
+func AlphanumObjKey(data map[string]string) bool {
+	regex, err := regexp.Compile("^[A-Za-z0-9_-]*$")
+	if err != nil {
+		return false
+	}
+
+	for _, v := range data {
+		isTrue := regex.MatchString(v)
+		if !isTrue {
+			return isTrue
+		}
+	}
+
+	return true
+}
